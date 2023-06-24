1181da05f58d06753d3e6d6a50670f803c5bd251
Aravinthan N <aravinthhd123@gmail.com>
2023-06-24 12:00:21 +0530

    added getters & setters & constructors

--- src/main/java/com/aravinth/taskminder/task/model/Task.java
+++ src/main/java/com/aravinth/taskminder/task/model/Task.java
@@ -7,4 +7,47 @@ public class Task {
   private String title;
   private String description;
   private User assignedTo;
+
+  public Task(String title, String description) {
+    this.title = title;
+    this.description = description;
+  }
+
+  public Task(String title, String description, User assignedTo) {
+    this.title = title;
+    this.description = description;
+    this.assignedTo = assignedTo;
+  }
+
+  public String getTaskId() {
+    return taskId;
+  }
+
+  public void setTaskId(String taskId) {
+    this.taskId = taskId;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public User getAssignedTo() {
+    return assignedTo;
+  }
+
+  public void setAssignedTo(User assignedTo) {
+    this.assignedTo = assignedTo;
+  }
 }
--- src/main/java/com/aravinth/taskminder/user/model/User.java
+++ src/main/java/com/aravinth/taskminder/user/model/User.java
@@ -1,7 +1,6 @@
 package com.aravinth.taskminder.user.model;
 
 import com.aravinth.taskminder.task.model.Task;
-
 import java.util.List;
 
 public class User {
@@ -11,4 +10,70 @@ public class User {
   private List<Role> roles;
   private List<Permission> permissions;
   private List<Task> taskList;
+
+  public User(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  public User(
+      String firstName,
+      String lastName,
+      List<Role> roles,
+      List<Permission> permissions,
+      List<Task> taskList) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.roles = roles;
+    this.permissions = permissions;
+    this.taskList = taskList;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getUserId() {
+    return userId;
+  }
+
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+
+  public List<Role> getRoles() {
+    return roles;
+  }
+
+  public void setRoles(List<Role> roles) {
+    this.roles = roles;
+  }
+
+  public List<Permission> getPermissions() {
+    return permissions;
+  }
+
+  public void setPermissions(List<Permission> permissions) {
+    this.permissions = permissions;
+  }
+
+  public List<Task> getTaskList() {
+    return taskList;
+  }
+
+  public void setTaskList(List<Task> taskList) {
+    this.taskList = taskList;
+  }
 }
